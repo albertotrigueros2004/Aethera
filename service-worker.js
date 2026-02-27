33d0f8b5c7010abf9fa09837fe2f25405831c55c
albertotrigueros2004 <alberto.trigueros2004@gmail.com>
2026-02-27 23:53:22 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("aethera-cache").then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/styles.css",
+        "/app.js",
+        "/manifest.json"
+      ]);
+    })
+  );
+});
